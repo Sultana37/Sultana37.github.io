eb99124eeec95cc67f1d7a736916c96d537aebcc
Esrat <imranzaman.318@gmail.com>
2021-03-27 02:17:11 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const package=document.getElementById("package");
+const headcontainer=document.getElementsByClassName("heading-container");
+function createcontainer(){
+
+console.log(package);
+}
+
+function headcontainer(){
+   console.log(heading-container);
+}
